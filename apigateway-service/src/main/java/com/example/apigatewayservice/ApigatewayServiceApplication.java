b796d54decb18adb5288008c4b36212a5cdcfba7
Sun <syseoz@naver.com>
2021-12-28 22:10:54 +0900

    add config

--- apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,6 +1,8 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -9,6 +11,12 @@ public class ApigatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
+    }
+
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository(){
+        return new InMemoryHttpTraceRepository();
     }
 
 
